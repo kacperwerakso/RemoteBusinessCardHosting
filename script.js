2b8d7b04e71e6b571bd50e857a1b8f768b4ec8a6
Kacper Werakso <werakso.kacper@gmail.com>
2024-12-18 23:50:37 +0100

    RWD update

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const nameElement = document.getElementById('fullName');
-    const fullName = 'Kacper Werakso';
-    let index = 0;
-  
-    function typeEffect() {
-      if (index < fullName.length) {
-        nameElement.textContent += fullName[index];
-        index++;
-        setTimeout(typeEffect, 150);
-      }
+  const nameElement = document.getElementById('fullName');
+  const fullName = 'Kacper Werakso';
+  let index = 0;
+
+  nameElement.classList.add('fullNameJs');
+
+  function typeEffect() {
+    if (index < fullName.length) {
+      nameElement.textContent += fullName[index];
+      index++;
+      setTimeout(typeEffect, 150);
     }
-  
-    typeEffect();
-  });
+  }
+
+  typeEffect();
+});
